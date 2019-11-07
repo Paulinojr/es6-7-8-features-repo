d0d4452f4871fe2fdf1ccc76f435846f4070616b
Paulino Jr <paulino.junior@outlook.com>
2019-11-07 16:02:32 -0300

    add rest operator

---
+++ rest-operator.js
@@ -0,0 +1,18 @@
+/*
+ * Sorry couldn't apply King of Fighters on this one ;(
+ */
+
+/*
+ * If you're writing a function and you're not
+ * sure how many things will come as parameter
+ * you can use rest operator like this! :)
+ */
+ function add(...nums){
+     /*
+      * Returns:
+      * [1, 2, 3, 4, 5]
+      */
+     console.log(nums)
+ }
+
+ add(1, 2, 3, 4, 5)
