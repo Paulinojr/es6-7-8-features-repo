5df37e60a69d48c2e01fcad5ffb6a65611f2eddb
Paulino Jr <paulino.junior@outlook.com>
2019-11-07 14:19:36 -0300

    Add for of loop

---
+++ for-of-loop.js
@@ -0,0 +1,23 @@
+let fullName = "Ryo Sakazaki";
+
+/* It is basically saying:
+*  "For each character of full name, do something"
+*/
+for (const char of fullName){
+    /*
+     * Returns:
+     * R
+     * y
+     * o
+     * 
+     * S
+     * a
+     * k
+     * a
+     * z
+     * a
+     * k
+     * i
+     */
+    console.log(char)
+}
