ebbacb8307694d0f44e0fe83807de5d6f73c8a67
Paulino Jr <paulino.junior@outlook.com>
2019-11-07 15:08:44 -0300

    add spread operator

---
+++ spread-operator.js
@@ -0,0 +1,13 @@
+let team1 = ["Leona", "Ralf Jones", "Clark Still"];
+/*
+ * The spread operator allows you to expand your 
+ * objects/iterables to others, in this case
+ * it is expanding team1 values to team2
+ */
+let team2 = [...team1];
+
+/*
+ * Returns:
+ * "Leona", "Ralf Jones", "Clark Still"
+ */
+console.log(example2)
