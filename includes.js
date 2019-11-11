046bae4326ec751bac0e7654b5a37cd0cf149b7d
Paulino Jr <paulino.junior@outlook.com>
2019-11-11 09:06:16 -0300

    Add includes

---
+++ includes.js
@@ -0,0 +1,12 @@
+// Includes allows us to tell whether or not an element exists under an array
+let fighterArray = ['Benimaru Nikaido', 'Blue Mary', 'Billy Kane'];
+
+/*
+ * Returns: false 
+ */
+console.log(fighterArray.includes('Geese Howard'))
+
+/*
+ * Returns: true 
+ */
+console.log(fighterArray.includes('Benimaru Nikaido'))
